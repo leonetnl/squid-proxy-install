a4b9bd309fd888a5d946565904d1cddc16ba3852
Leon Boon <info@leonet.nl>
2021-11-04 15:25:53 +0100

    Print proxy

--- squid-add-user.sh
+++ squid-add-user.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-############################################################
-# Squid Proxy Installer
-# Author: Yujin Boby
-# Email: info@serverok.in
-# Github: https://github.com/serverok/squid-proxy-installer/
-# Web: https://serverok.in/squid
-############################################################
-# For paid support, contact
-# https://serverok.in/contact
-############################################################
-
 if [ `whoami` != root ]; then
 	echo "ERROR: You need to run the script as user root or add sudo before command."
 	exit 1
@@ -22,10 +11,6 @@ fi
 
 read -e -p "Enter Proxy username: " proxy_username
 
-if [ -f /etc/squid/passwd ]; then
-    /usr/bin/htpasswd /etc/squid/passwd $proxy_username
-else
-    /usr/bin/htpasswd -c /etc/squid/passwd $proxy_username
-fi
 
+/usr/bin/htpasswd -p /etc/squid/passwd $proxy_username
 systemctl reload squid
