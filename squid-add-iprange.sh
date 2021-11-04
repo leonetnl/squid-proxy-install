f14ccdfac6e68777ffff0b3c34c920358f8d3916
Leon Boon <info@leonet.nl>
2021-11-04 13:51:28 +0100

    Changed delimiter

--- squid-add-iprange.sh
+++ squid-add-iprange.sh
@@ -4,7 +4,7 @@ echo -e "\e[92mEnter ip range including netmask e.g. (192.168.1.1/28)"
 echo -e "\033[00m";
 read ip
 ips=$(prips $ip | sed -e '1d; $d' | awk -vORS=, '{ print $1 "/32" }' | sed 's/,$/\n/')
-sed -i "s/addresses: \[\(.*\)\]/addresses: [$ips]/g" 60-static.yaml
+sed -i "s~addresses: \[\(.*\)\]~addresses: [$ips]~g" 60-static.yaml
 
 #generate ip list
 if [[ -d /etc/netplan/ ]]; then
