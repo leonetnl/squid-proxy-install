62d4ee70e06ed4bf48142b315f56da0fdbddd427
Leon Boon <l.boon@dictu.nl>
2021-11-05 09:16:23 +0100

    add fi instead of else

--- squid-add-user.sh
+++ squid-add-user.sh
@@ -13,8 +13,8 @@ read -e -p "Enter Proxy username: " proxy_username
 
 if grep -q "${proxy_username}:" /etc/squid/passwd; then
     echo "Username already exists"
-    exit
-else
+    exit 1
+fi
 
 read -e -p "Enter Proxy password: " proxy_password
 read -e -p "IP from (e.g. 192.168.1.1):" proxy_ip_from
