e2f736e4436660f2c161cf8e576f38695b1eaf62
Leon Boon <l.boon@dictu.nl>
2021-11-05 13:47:09 +0100

    Remove user at job

--- squid-delete-user.sh
+++ squid-delete-user.sh
@@ -4,7 +4,7 @@ if [ `whoami` != root ]; then
 	echo "ERROR: You need to run the script as user root or add sudo before command."
 	exit 1
 fi
-                                                                         
+
 if test -n "${1-}"; then
   	user=$1
 else
@@ -13,5 +13,21 @@ fi
 
 sed -i "/${user}:/d" /etc/squid/passwd
 sed -i "/ ${user}/d" /etc/squid/users.conf
+
+# loop trough queue
+queueids=$(atq | cut -d$'\t' -f1)
+arr=($queueids)
+for id in ${arr[@]}; do
+	qu=$(at -c $id | grep "squid-delete-user.sh" | awk '{print $2}')
+	
+	if [[ "$qu" == "$user" ]]; then
+		echo "Removing job $id"
+		atrm $id
+	fi
+done
+
 echo "Available users:"
 cat /etc/squid/passwd
+
+
+
