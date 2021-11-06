b88695de7791b5b5395bc4328cbba7139f8d1e1b
Leon Boon <l.boon@dictu.nl>
2021-11-06 14:02:37 +0100

    add user from command line

--- squid-add-user.sh
+++ squid-add-user.sh
@@ -9,16 +9,29 @@ if [ ! -f /usr/bin/htpasswd ]; then
     exit 1
 fi
 
-read -e -p "Enter Proxy username: " proxy_username
+if  test -n "${1-}" && 
+    test -n "${2-}" &&
+    test -n "${3-}" && 
+    test -n "${4-}" &&
+    test -n "${5-}"; then
+    proxy_username=$1
+    proxy_password=$2
+    proxy_ip_from=$3
+    proxy_ip_to=$4
+    expire_days=$5
+else
+    read -e -p "Enter Proxy username: " proxy_username
+    read -e -p "Enter Proxy password: " proxy_password
+    read -e -p "IP from (e.g. 192.168.1.1):" proxy_ip_from
+    read -e -p "IP to (e.g. 192.168.1.10):" proxy_ip_to
+    read -e -p "In how many days will this user expire? 0 for not expiring" expire_days
+fi
+
 
 if grep -q "${proxy_username}:" /etc/squid/passwd; then
     echo "Username already exists"
     exit 1
 fi
-
-read -e -p "Enter Proxy password: " proxy_password
-read -e -p "IP from (e.g. 192.168.1.1):" proxy_ip_from
-read -e -p "IP to (e.g. 192.168.1.10):" proxy_ip_to
 
 IP_ALL=$(/sbin/ip -4 -o addr show scope global | awk '{gsub(/\/.*/,"",$4); print $4}')
 IP_ALL_ARRAY=($IP_ALL)
@@ -29,14 +42,11 @@ if !( ( echo ${IP_ALL_ARRAY[@]} | grep -qw $proxy_ip_from ) && ( echo ${IP_ALL_A
   exit 1
 fi
 
-read -p "Will this user expire? (y/n)" -n 1 -r
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    echo
-    read -e -p "In how many days? " expire_days
-    echo "${PWD}/squid-delete-user.sh ${proxy_username}" | at "now + ${expire_days} minute"
-    echo
+
+if [ $expire_days -gt 0 ]; then
+  echo "${PWD}/squid-delete-user.sh ${proxy_username}" | at "now + ${expire_days} minute"
 fi
+
 
 ips=$(prips $proxy_ip_from $proxy_ip_to)
 
