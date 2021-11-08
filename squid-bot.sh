b559af9d71cf0ee483db319537bd77585e3c09d7
Leon Boon <info@leonet.nl>
2021-11-08 13:18:07 +0100

    install deps

--- squid-bot.sh
+++ squid-bot.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+apt install python3-pip
+pip install -U discord.py
 pip install python-dotenv
 ps ax | grep squid-bot.py | awk '{ print $1 }' | xargs kill -9
 nohup python3 ./squid-bot.py &
