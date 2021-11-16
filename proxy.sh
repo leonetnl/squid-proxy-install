dd4b1144ca0912266eb9f5a518a5cdef8e890238
Leon Boon <info@leonet.nl>
2021-11-16 15:32:12 +0100

    nohup

--- proxy.sh
+++ proxy.sh
@@ -331,7 +331,7 @@ configureBot() {
 
 startBot() {
     ps ax | grep squid-bot.py | awk '{ print $1 }' | xargs kill -9
-    nohup python3 ./squid-bot.py &
+    nohup ./squid-bot.py &
 }
 
 #########################################################################################################
