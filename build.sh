611c6f67def4e5478aeccdb3111289d375f90bb2
Leon Boon <info@leonet.nl>
2021-11-16 16:19:12 +0100

    build and get

--- build.sh
+++ build.sh
@@ -6,3 +6,6 @@ rm ./proxy.sh.x.c
 cp ./netplan.yaml ./build/netplan.yaml
 cp ./squid.conf ./build/squid.conf
 cp ./squid-bot.py ./build/squid-bot.py
+
+
+# scp root@185.142.27.234:/root/squid-proxy-install/app.zip ./
